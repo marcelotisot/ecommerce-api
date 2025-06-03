ef7a91ce75360a76bd0e0043bf3e81f0040550e9
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2025-06-03 05:09:47 -0300

    (test: auth, products, users) fix dependencias

--- src/modules/products/controllers/products.controller.spec.ts
+++ src/modules/products/controllers/products.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from '../services/products.service';
+import { CategoriesService } from '../../../modules/categories/services/categories.service';
+import { Product } from '../entities/product.entity';
+import { Category } from '../../../modules/categories/entities/category.entity';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -8,7 +12,18 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        CategoriesService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: {}
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: {}
+        }
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
--- src/modules/products/services/products.service.spec.ts
+++ src/modules/products/services/products.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
+import { CategoriesService } from '../../../modules/categories/services/categories.service';
+import { Product } from '../entities/product.entity';
+import { Category } from '../../../modules/categories/entities/category.entity';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        CategoriesService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: {}
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: {}
+        }
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
